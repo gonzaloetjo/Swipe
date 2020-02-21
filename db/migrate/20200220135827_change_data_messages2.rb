79b5e45eb28bd3dc8761400c689a8bff358885a4
Alvaro Vega <nunyvega@hotmail.com>
2020-02-21 20:35:23 +0100

    offer page updated, it's now sexy

--- db/migrate/20200220135827_change_data_messages2.rb
+++ db/migrate/20200220135827_change_data_messages2.rb
@@ -3,6 +3,6 @@ class ChangeDataMessages2 < ActiveRecord::Migration[5.2]
     remove_column :messages, :receiving_user_id, :integer
     remove_column :messages, :sending_user_id, :integer
     add_column :messages, :user_id, :integer, :references => "users"
-    add_column :messages, :product_id, :integer, references: => "products"
+    add_column :messages, :product_id, :integer, :references => "products"
   end
 end
