fec1505de7b5771c9313152e6523d7e1011913ea
Gonzalo Etse <gonzaloetjo@gmail.com>
2020-02-25 12:43:23 +0100

    Por favor funciona

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   #def favorites
   #  @favorites = Favorite.where(user_id: current_user.id)
   #end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :avatar])
+  end
 end
